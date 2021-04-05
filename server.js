5aff320cb2a1c3d1f178baf457b8fef8b1984746
Ike-Njoku David <ike-njokudc@Ike-Njokus-MacBook-Pro.local>
2021-04-05 12:00:35 +0100

    teris app can now draw tetraminoes

--- server.js
+++ server.js
@@ -2,17 +2,52 @@
 const PORT = process.env.PORT || 1000;
 const nodemon = require('nodemon')
 const http = require('http');
+const fileSystem = require('fs');
 http.createServer(
     (request, response) => {
 
-        if (request.url == "/") response.end(
-            "welcome to the tetris game home page???"
-        );
+        try {
 
-        if (request.url == "/tetris-game") {
-            response.writeHead(200).end(
-                'app is working'
-            )
+            if (request.url == "/") response.end(
+                "welcome to the tetris game home page???"
+            );
+
+
+            // loading the stylesheet
+            if (request.url.includes("tetris.style.css")) {
+                fileSystem.readFile("./static/tetris/tetris.style.css", (error, content) => {
+                    if (error) response.end(error.message);
+                    else response.end(content);
+                })
+            }
+
+            // load the js file
+            if (request.url.includes("tetris.app.js")) {
+                fileSystem.readFile("./static/tetris/tetris.app.js", (error, content) => {
+                    if (error) response.end(error.message);
+                    else response.end(content);
+                })
+            }
+
+            // load the html file
+            if (request.url == "/tetris") {
+                // set the response status and the header
+                response.writeHead(200, {
+                    "Content-Type": "text/html"
+                })
+
+                fileSystem.readFile("./static/tetris/tetris.index.html", (error, content) => {
+                    if (error) response.end(error.message);
+                    else response.end(content);
+                })
+
+            }
+
+        } catch (error) {
+
+            response.end(error.message);
+
         }
+
     }
-).listen(PORT, () => console.log(`jsTetris is running on port ${PORT}`));
+).listen(PORT, () => console.log(`Game server running on port: ${PORT}`));
--- static/tetris/tetris.app.js
+++ static/tetris/tetris.app.js
@@ -1,7 +1,30 @@
 // Tetris game app written by ike-njoku david in april 2021
 const totalGrids = 300;
+// if the total columns is changed, make complementary changes in tetris.style.css
+const totalColumns = 15;
 const gridContainer = document.querySelector('[data-gridContainer]');
+// define the array of tetraminoes
+const tetraminoes = [
+    ["T-tetramino", [1, 15, 16, 17]],
+
+    ["L-tetramino", [7, 22, 37, 38],
+        [1, 2, 3, 18],
+        [8, 23, 24, 25]
+    ],
+
+]
+
+
+
 class Tetris {
+
+    // current tetramino
+    currentTetramino;
+
+    // current position
+
+    // game score
+    score;
 
     // ------------Game Environment ----------
 
@@ -10,10 +33,22 @@ class Tetris {
         // create small grids
 
         let i = 0;
+        // initialize the rows (starting at -1 because the row value increments and it's meant to be zero based)
+        let row = -1;
         while (i < totalGrids) {
+            // create grids
             let createGrid = document.createElement('div');
+            // --- add attrubutes to the grids
+            // add grid numbers
+            createGrid.setAttribute('data-grid', i);
+            // add column nuners
+            let column = i % totalColumns;
+            createGrid.setAttribute('column', column);
+            // add rows to the grids
+            if (column == 0) row += 1;
+            createGrid.setAttribute('row', row);
+            // append the grids to the grid container
             gridContainer.appendChild(createGrid);
-            console.log(i);
             i++;
         }
 
@@ -38,6 +73,23 @@ class Tetris {
     // erase tetramino
 
     // draw tetramino
+    drawTetramino() {
+        // find a starting point
+        const grids = document.querySelectorAll('[data-grid]');
+        // reference point to start drawing from
+        let referencePoint = 12;
+        // loop through the grids to find which grids would become coordinates by adding the reference point to them
+        grids.forEach((grid) => {
+            // loop through the predefined tetramino coordinates and add the reference point to them to figure where they should be drawn
+            tetraminoes[0][1].forEach((tetraminoCoordinate) => {
+                // if the grid number matches the ones returned after adding the reference point, colour it;
+                if (grid.getAttribute('data-grid') == tetraminoCoordinate + referencePoint)
+                    grid.classList.add(tetraminoes[0][0]);
+            })
+        });
+
+
+    }
 
     // move trtramino left
 
@@ -56,3 +108,4 @@ class Tetris {
 
 const tetris = new Tetris();
 tetris.buildGameEnvironment();
+tetris.drawTetramino();
