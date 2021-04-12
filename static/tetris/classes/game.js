d8c702b30ba785eb9d01733a4d053111baf8ff98
ike-Njoku <ikenjokudc@gmail.com>
2021-04-12 11:27:33 +0100

    restructured sub-directories

---
+++ static/tetris/classes/game.js
@@ -0,0 +1,21 @@
+export class Game {
+
+    // build environment;
+    build() {
+        console.log('hello world');
+        // total number of grids that make up the game environment
+        const totalGrids = 300;
+        // total number of grid columns
+        const totalColumns = 15;
+    }
+
+    // start game
+    startGame() {}
+
+    // pause game
+    pauseGame() {}
+
+    //end game 
+    endGame() {}
+
+}
---
+++ static/tetris/classes/tetris.js
@@ -0,0 +1,16 @@
+export class Tetris {
+    // select Tetramino
+    selectTetramino() {}
+
+    // rotate Tetramino
+    rotateTetramino() {}
+
+    //move tetramino down
+    moveTetraminoDown() {}
+
+    // move tetramino right
+    moveTetraminoRight() {}
+
+    // move tetramino left 
+    moveTetraminoRIght() {}
+}
